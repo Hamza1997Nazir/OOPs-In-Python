d526e4f1559d59cffcaa470dc49a01440e94cb71
Hamza <hamza40nazir@gmail.com>
2020-08-26 21:32:58 +0500

    Starting Python OOP

---
+++ Ch 1/definition_start.py
@@ -0,0 +1,22 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Basic class definitions
+
+
+# TODO: create a basic class
+
+class Book:
+	def __init__(self,title):
+		self.title = title
+
+
+
+
+book1 = Book("Cat")
+book2 = Book("Sparrow")
+
+print(book1.title)
+print(book2.title)
+# TODO: create instances of the class
+
+
+# TODO: print the class and property
